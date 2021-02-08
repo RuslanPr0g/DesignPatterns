e153fab6ac81029299db44eef020ff558ac7d18d
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-08 19:37:27 +0300

    add abstract factory

---
+++ AbstractFactory/AbstractFactory/Factory/AbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory
+{
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+}
