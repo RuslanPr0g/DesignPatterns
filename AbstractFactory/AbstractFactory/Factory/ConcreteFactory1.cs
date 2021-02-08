84512250a082e06c3bf2611f08e91597062d85b6
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-08 19:38:24 +0300

    add concrete factory

---
+++ AbstractFactory/AbstractFactory/Factory/ConcreteFactory1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Factory
+{
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Factory/ConcreteFactory2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Factory
+{
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+}
