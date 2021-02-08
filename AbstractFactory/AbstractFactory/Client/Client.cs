69ae328022c160a147a1f8edf0eaa4600a266eb7
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-08 19:39:58 +0300

    add client

---
+++ AbstractFactory/AbstractFactory/Client/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Client
+{
+    class Client
+    {
+        private AbstractProductA abstractProductA;
+        private AbstractProductB abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            abstractProductB = factory.CreateProductB();
+            abstractProductA = factory.CreateProductA();
+        }
+
+        public void Run()
+        { }
+    }
+}
