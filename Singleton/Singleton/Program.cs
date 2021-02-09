3ee7b306859a033e4aa47e0069dbea6b737ea3b5
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-09 09:55:45 +0300

    Thread problem

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Singleton
 {
@@ -6,12 +7,17 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            (new Thread(() =>
+            {
+                Computer comp2 = new Computer();
+                comp2.OS = OS.getInstance("Windows 10");
+                Console.WriteLine(comp2.OS.Name);
+            })).Start();
+
             Computer comp = new Computer();
             comp.Launch("Windows 8.1");
             Console.WriteLine(comp.OS.Name);
-
-            comp.OS = OS.getInstance("Windows 10");
-            Console.WriteLine(comp.OS.Name);
+            Console.ReadLine();
 
             Console.ReadLine();
         }
