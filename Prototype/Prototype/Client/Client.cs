a5910126c96ba77513956c031cc7f2e7673e3b35
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-11 00:57:24 +0300

    prototype essense

---
+++ Prototype/Prototype/Client/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace Prototype
+{
+    class Client
+    {
+        void Operation()
+        {
+            Prototype prototype = new ConcretePrototype1(1);
+            Prototype clone = prototype.Clone();
+            prototype = new ConcretePrototype2(2);
+            clone = prototype.Clone();
+        }
+    }
+}
---
+++ Prototype/Prototype/Prototype/Prototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    abstract class Prototype
+    {
+        public int Id { get; private set; }
+        public Prototype(int id)
+        {
+            this.Id = id;
+        }
+        public abstract Prototype Clone();
+    }
+}
