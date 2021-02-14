2a9518dae86978297e939c72144b026153440c56
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-14 21:37:20 +0300

    TemplateMethod essense

---
+++ Behavioral Patterns/TemplateMethod/TemplateMethod/Essense/AbstractClass.cs
@@ -0,0 +1,13 @@
+﻿namespace TemplateMethod
+{
+    abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            Operation1();
+            Operation2();
+        }
+        public abstract void Operation1();
+        public abstract void Operation2();
+    }
+}
---
+++ Behavioral Patterns/TemplateMethod/TemplateMethod/Essense/ConcreteClass.cs
@@ -0,0 +1,13 @@
+﻿namespace TemplateMethod
+{
+    class ConcreteClass : AbstractClass
+    {
+        public override void Operation1()
+        {
+        }
+
+        public override void Operation2()
+        {
+        }
+    }
+}
