48d626425ad66155589633da9d0feac911623958
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-23 22:09:26 +0300

    Interpeter: create

---
+++ Behavioral Patterns/Interpretor/Interpretor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interpretor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
